662cc928739b74b4706ed71bb0b1701bc8301cea
Matt Burger <matt.burger1@outlook.com>
2019-07-08 11:24:06 -0700

    components created

--- tm_frontend/src/components/app.js
+++ tm_frontend/src/components/app.js
@@ -1,25 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './app.scss';
+
+const API = process.env.TASKMASTER_DEV_API;
+
+function Task() {
+
+  const [task, setTask] = useState([]);
+
+  const _getTask = () => {
+    fetch( API, {
+      mode:'cors',
+    })
+    .then( data => data.json() )
+    .then( tsk => setTask(tsk) )
+    .catch( console.error );
+
+  };
+
+  // const _advanceTask = (e) => {
+  //   e.preventDefault();
+  //   let id = e.target.id;
+
+  //   fetch( `${API}/${id}/status`, {
+  //     mode:'cors',
+  //     method: 'PATCH'
+  //   })
+  //   .then(data => data.json())
+  //   .then(task => {
+  //     setTask( task.map( (entry) => {
+  //         return entry.id === id ? task : entry;
+  //       }
+  //     ));
+  //   })
+  //   .catch( console.error );
+
+  // };
+
+  useEffect(_getTask, []);
+  
+
+  return (
+    <ul>
+      {task.map( (task) =>
+        <li className={`task-${task.status}`} key={task.id}>
+          <details>
+            <summary>
+              <span>{task.title}</span>
+            </summary>
+            <Details task={task} />
+          </details>
+        </li>
+      )}
+    </ul>
+  )
+}
+
+function Details(props) {
+
+  const _advanceTask = (e) => {
+    e.preventDefault();
+    let id = e.target.id;
+
+    fetch( `${API}/${id}/status`, {
+      mode:'cors',
+      method: 'PATCH'
+    })
+    .then(data => data.json())
+    .then(task => {
+      setTask( task.map( (entry) => {
+          return entry.id === id ? task : entry;
+        }
+      ));
+    })
+    .catch( console.error );
+
+  };
+
+  
+  let tsk = props.task || [];
+  return (
+    <section>
+      {tsk.map( (item,idx) =>
+        <div>
+          <span>{item.description}</span>
+          <span>{item.assignee}</span>
+          <span id={tsk.id} onClick={_advanceTask}>{tsk.status}</span>
+          {/* <span>{item.status}</span> */}
+        </div>
+      )}
+    </section>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>Tasks</header>
+      <main>
+        <Task />
+      </main>
+      <footer></footer>
+      </>
   );
 }
 
